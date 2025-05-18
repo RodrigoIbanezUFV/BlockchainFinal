1547d4a6a2e9ee42fcd7c624c2da585f231fb6e9
pablo <pablorostejerina568@gmail.com>
2025-05-18 17:08:54 +0200

    actualizacion de branch y funcionalidades en token festival

--- Contracts/TokenFestival.sol
+++ Contracts/TokenFestival.sol
@@ -1,31 +1,37 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
-import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.0.1/contracts/token/ERC1155/ERC1155.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.0.1/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.0.1/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.0.1/contracts/access/Ownable.sol";
 
-contract TokenFestival is ERC1155, ERC1155Burnable, ERC1155Supply, Ownable {
+contract TokenFestival is ERC1155,Ownable {
     uint256 public constant ENTRADA_GENERAL = 0;
     uint256 public constant ENTRADA_VIP = 1;
     uint256 public constant ENTRADA_BACKSTAGE = 2;
     uint256 public constant ENTRADA_INVITADO = 3;
 
-    constructor() ERC1155("https://raw.githubusercontent.com/RodrigoIbanezUFV/BlockchainFinal/main/metadata/{id    function _beforeTokenTransfer(
-        address operator,
-        address from,
-        address to,
-        uint256[] memory ids,
-        uint256[] memory amounts,
-        bytes memory data
-    ) internal override(ERC1155, ERC1155Supply) {
-        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
-    }
-}.json") {
+    constructor() 
+        ERC1155("https://raw.githubusercontent.com/RodrigoIbanezUFV/BlockchainFinal/master/metadata/{id}.json")
+        Ownable(msg.sender)
+    {
         _mint(msg.sender, ENTRADA_GENERAL, 1, "");
         _mint(msg.sender, ENTRADA_VIP, 1, "");
         _mint(msg.sender, ENTRADA_BACKSTAGE, 1, "");
         _mint(msg.sender, ENTRADA_INVITADO, 1, "");
+    }
+
+    function _update(
+    address from,
+    address to,
+    uint256[] memory ids,
+    uint256[] memory values // Removed data here
+) internal override {
+    super._update(from, to, ids, values);
+}
+
+    function burn(uint256 id) public onlyOwner {
+   _burn(msg.sender, id, 1); // Corrected syntax with three arguments (msg.sender, id, and 1)
     }
 }
